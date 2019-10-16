9796f2c2acf1cdd634e6d9cbbe2bae9026a7bbec
shakerbaker78 <bakeamym@yahoo.com>
2019-10-16 09:59:49 -0600

    push

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -11,6 +11,10 @@ function loadDate(){
     
     document.getElementById("year").innerHTML = year; 
     document.getElementById("lastModified").innerHTML = day+", "+dateNumber+" "+month+" "+year;
+
+    if (day == "Friday") {
+        document.getElementById("pancake").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+    }
     }
 
 function toggleMenu (){

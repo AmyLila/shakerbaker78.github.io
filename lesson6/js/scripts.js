fef50cd20a0577ce02421f8859d299968e9184c8
shakerbaker78 <bakeamym@yahoo.com>
2019-10-21 17:42:14 -0600

    lesson 6

---
+++ lesson6/js/scripts.js
@@ -0,0 +1,23 @@
+function loadDate(){
+    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+    var dateObject = new Date();
+    var day = days[dateObject.getDay()];
+    var dateNumber =dateObject.getDate();
+    var month = monthNames[dateObject.getMonth()];  
+    var year = dateObject.getFullYear();
+    
+    document.getElementById("year").innerHTML = year; 
+    document.getElementById("lastModified").innerHTML = day+", "+dateNumber+" "+month+" "+year;
+
+    if (day == "Friday") {
+        document.getElementById("pancake").innerHTML = "Saturday = Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion.";
+    }
+    }
+
+function toggleMenu (){
+    console.log(document.getElementById("primaryNav").classList);
+    document.getElementById("primaryNav").classList.toggle("hide");
+}    

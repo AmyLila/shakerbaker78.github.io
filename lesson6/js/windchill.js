f925fb7bf181668f0b9a198893df1066b19e66d5
shakerbaker78 <bakeamym@yahoo.com>
2019-10-22 11:39:46 -0600

    js

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,12 +1,14 @@
 function calcWindChill() {
 
-    var tempF = parseFloat(document.getElementById('temp').value);
-    var speed = parseFloat(document.getElementById('windspeed').value);
+    var tempF = parseFloat(document.getElementById('temp').textContent);
+    var speed = parseFloat(document.getElementById('windspeed').textContent);
+    console.log(tempF, speed);
     var F = windChill(tempF, speed);
-    document.getElementById('windchill').innerHTML = F + "&#8457;";
+    document.getElementById('windchill').innerHTML = F.toFixed(1) + "&#8457;";
     }
     
     function windChill(tempF, speed) {
     var wFactor = (35.74 + (0.6215*tempF)- (35.75*(Math.pow(speed, 0.16))) + (0.4275*tempF*Math.pow(speed, 0.16)));
     return wFactor;
     }
+    calcWindChill();

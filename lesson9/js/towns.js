f13157014057b614511e69dec5194bd7afbdc88b
shakerbaker78 <bakeamym@yahoo.com>
2019-11-20 21:10:17 -0700

    js

--- lesson9/js/towns.js
+++ lesson9/js/towns.js
@@ -17,83 +17,36 @@ fetch(requestURL)
           
             let card = document.createElement('section');
             let townInfo = document.createElement('div');
-            let h2 = document.createElement('h2');
-            h2.textContent = towns[i].name + ' ' + towns[i].lastname;
-            card.appendChild(h2);
+            let townPicture = document.createElement('div');
+            
+            let name = document.createElement('h3');
+            name.textContent = towns[i].name;
+            card.appendChild(name);
 
-            let p = document.createElement('p');
-            p.textContent = "Date of Birth: " + towns[i].birthdate;
-            card.appendChild(p);
+            let motto = document.createElement('h4');
+            motto.textContent = towns[i].motto;
+            card.appendChild(motto);
 
-            let d = document.createElement('p');
-            d.textContent = "Place of Birth: " +  towns[i].birthplace;
-            card.appendChild(d);
+            let founded = document.createElement('p');
+            founded.textContent = "Year Founded: " + towns[i].yearFounded;
+            card.appendChild(founded);
+
+            let pop = document.createElement('p');
+            pop.textContent = "Population: " +  towns[i].currentPopulation;
+            card.appendChild(pop);
+
+            let rainFall = document.createElement('p');
+            rainFall.textContent = "Annual Rain Fall: " +  towns[i].currentPopulation;
+            card.appendChild(rainFall);
 
             let image = document.createElement('img');
-            image.setAttribute('src', towns[i].imageurl);
-            image.setAttribute('alt', towns[i].name + ' ' + towns[i].lastname + " - " + towns[i].order);
+            image.setAttribute('src', "images/" + towns[i].photo);
+            image.setAttribute('alt', 'picture of ' + towns[i].name + ', Idaho');
             card.appendChild(image);
            
-            document.querySelector('div.cards').appendChild(card);
+            document.querySelector('div.town-data').appendChild(card);
         }
       }
   });
-  
 
-
-  
-
-
-
-
-
-
-
-// fetch(requestURL)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (jsonObject) {
-//     console.table(jsonObject);  // temporary checking for valid response and data parsing
-//     const towns = jsonObject['towns'];
-
-//     for (let i = 0; i < towns.length; i++ ) {
-//       if (towns[i].name == "Fish Haven" ||
-//           towns[i].name == "Preston" ||
-//           towns[i].name == "Soda Springs")
-//       {     
-//         let card = document.createElement('section'); 
-//         let townInfo = document.createElement('div');
-//         let townImage = document.createElement('div');
-//         let name = document.createElement('h3');
-//         let motto = document.createElement('p');
-//         let founded = document.createElement('p');
-//         let population = document.createElement('p');
-//         let rainfall = document.createElement('p');
-//         let photo = document.createElement('img');
-
-//         townInfo.className = "town-info";
-//         townImage.className = "town-image";
-//         name.textContent = towns[i].name;
-//         motto.textContent = towns[i].motto;
-//         motto.className = "motto";
-//         founded.textContent = 'Year Founded: ' + towns[i].yearFounded;
-//         population.textContent = 'Population: ' + towns[i].currentPopulation;
-//         rainfall.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
-//         photo.setAttribute('src', "images/" + towns[i].photo);
-//         photo.setAttribute('alt', 'picture of ' + towns[i].name + ', Idaho');
-
-//         townInfo.appendChild(name);
-//         townInfo.appendChild(motto);
-//         townInfo.appendChild(founded);
-//         townInfo.appendChild(population);
-//         townInfo.appendChild(rainfall);
-//         townImage.appendChild(photo);
-//         card.appendChild(townInfo);
-//         card.appendChild(townImage);
-
-//         document.querySelector('div.town-data').appendChild(card);
-//       }
-//     }
-//   });
 

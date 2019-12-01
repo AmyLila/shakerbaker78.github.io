6aa3ea6409575bc8e9d47c8eefb4bea56efecdfb
shakerbaker78 <bakeamym@yahoo.com>
2019-11-30 18:16:53 -0700

    push

--- lesson11/js/scripts.js
+++ lesson11/js/scripts.js
@@ -33,19 +33,27 @@ function toggleMenu (){
 $(function () {
 
     var galleryImage = $(".homeHeroContainer").find("img").first();
+    var altText = [
+      "barn under a night sky",
+      "barn in an autum forest"];
   
     var images = [
       "images/fish-haven-large.jpg",
       "images/sodahero.jpg"];
 
     var i = 0;
+    
+    var t = 0;
+
   
     setInterval(function() {
 
       i = (i + 1) % images.length; 
+      t = (t + 1) % altText.length;
  
       galleryImage.fadeOut(function() {
         $(this).attr("src", images[i],);
+        $(this).attr("alt", altText[t],);
         $(this).fadeIn();
       });
       console.log(galleryImage.attr("src"));

185970197075b6ab6e7ffb7fbbf02990886a9af3
shakerbaker78 <bakeamym@yahoo.com>
2019-12-07 13:55:26 -0700

    header work

---
+++ temple-inn/js/scripts.js
@@ -0,0 +1,8 @@
+// show/hide Navigation 
+
+function toggleMenu (){
+    console.log(document.getElementById("primaryNav").classList);
+    document.getElementById("primaryNav").classList.toggle("hide");
+
+
+}

805f01bf7416199b0497d0ccda2360a0b913a9fb
shakerbaker78 <bakeamym@yahoo.com>
2019-12-11 09:43:10 -0700

    js

--- temple-inn/js/scripts.js
+++ temple-inn/js/scripts.js
@@ -7,42 +7,42 @@ function toggleMenu (){
 
 }
 // Home Page Slider
-// $(function () {
+$(function () {
 
-//     var galleryImage = $(".homeHeroImage").find("img").first();
-//     var altText = [
-//       "Rome Italy Temple",
-//       "Salt Lake City Temple",
-//       "Bern Switzerland Temple",
-//       "Newport Beach California Temple",
-//       "Nauvoo Temple"
-//     ];
+    var galleryImage = $(".homeHeroImage").find("img").first();
+    var altText = [
+      "Rome Italy Temple",
+      "Salt Lake City Temple",
+      "Bern Switzerland Temple",
+      "Newport Beach California Temple",
+      "Nauvoo Temple"
+    ];
   
-//     var images = [
-//         "images/rome-temple-low-res.jpg",
-//         "images/slc-temple-low-res.jpg", 
-//         "images/bern-switzerland-temple-lds-low-res.jpg",
-//         "images/newport-beach-california-temple-low-res.jpg",
-//         "images/nauvoo-temple-low-res.jpg"
-//     ];
+    var images = [
+        "images/rome-temple-low-res.jpg",
+        "images/slc-temple-low-res.jpg", 
+        "images/bern-switzerland-temple-lds-low-res.jpg",
+        "images/newport-beach-california-temple-low-res.jpg",
+        "images/nauvoo-temple-low-res.jpg"
+    ];
 
-//     var i = 0;
+    var i = 0;
     
-//     var t = 0;
+    var t = 0;
 
   
-//     setInterval(function() {
+    setInterval(function() {
 
-//       i = (i + 1) % images.length; 
-//       t = (t + 1) % altText.length;
+      i = (i + 1) % images.length; 
+      t = (t + 1) % altText.length;
  
-//       galleryImage.fadeOut(function() {
-//         $(this).attr("src", images[i],);
-//         $(this).attr("alt", altText[t],);
-//         $(this).fadeIn();
-//       });
-//       console.log(galleryImage.attr("src"));
-//     }, 6000);
+      galleryImage.fadeOut(function() {
+        $(this).attr("src", images[i],);
+        $(this).attr("alt", altText[t],);
+        $(this).fadeIn();
+      });
+      console.log(galleryImage.attr("src"));
+    }, 6000);
 
    
-//   });
+  });

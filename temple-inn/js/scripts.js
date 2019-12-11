dda08f039f730a65f5f95a3253d125a5a3ac3467
shakerbaker78 <bakeamym@yahoo.com>
2019-12-11 10:25:39 -0700

    push

--- temple-inn/js/scripts.js
+++ temple-inn/js/scripts.js
@@ -51,14 +51,79 @@ function toggleMenu (){
 
 //----------------------Home Page Info ----------------------------------
 
-const apiURL = "https://shakerbaker78.github.io./temple-inn/data/temple-info.json";
-fetch(apiURL)
-    .then((response) => response.json())
-    .then((jsObject) => {
-    console.log(jsObject);
+const templeURL = "https://shakerbaker78.github.io./temple-inn/data/temple-info.json";
+fetch(templeURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject); //temporary checking for valid response and data parsing
+    const temple = jsonObject['temples'];
+
+    for (let i = 0; i < temples.length; i++ ) {
+                  if (temples[i].name == "Rome Italy Temple")
+                  {  
 
     document.getElementById("name").textContent = jsObject.name;
     document.getElementById("temp").textContent = jsObject.main.temp;
     document.getElementById("humid").textContent = jsObject.main.humidity;
     document.getElementById("windspeed").textContent = jsObject.wind.speed;
  });
+
+
+
+//         for (let i = 0; i < temples.length; i++ ) {
+//           if (temples[i].name == "Fish Haven" ||
+//               temples[i].name == "Preston" ||
+//               temples[i].name == "Soda Springs")
+//           {    
+          
+//             let card = document.createElement('section');
+//             let townInfo = document.createElement('div');
+//             let name = document.createElement('h3');
+//             let motto = document.createElement('h4');
+//             let founded = document.createElement('p');
+//             let pop = document.createElement('p');
+//             let rainFall = document.createElement('p');
+//             let townPicture = document.createElement('div');
+//             let image = document.createElement('img');
+            
+            
+            
+//             townInfo.className = "town";
+//             name.textContent = temples[i].name;
+//             townInfo.appendChild(name);
+
+            
+//             motto.textContent = temples[i].motto;
+//             motto.className = "motto";
+//             townInfo.appendChild(motto);
+
+            
+//             founded.textContent = "Year Founded: " + temples[i].yearFounded;
+//             townInfo.appendChild(founded);
+
+            
+//             pop.textContent = "Population: " +  temples[i].currentPopulation;
+//             townInfo.appendChild(pop);
+
+            
+//             rainFall.textContent = "Annual Rain Fall: " +  temples[i].averageRainfall;
+//             townInfo.appendChild(rainFall);
+
+//             // townPicture.className = "pic";
+            
+            
+//             image.setAttribute('src', "images/" + temples[i].photo);
+//             image.setAttribute('alt', 'picture of ' + temples[i].name + ', Idaho');
+//             townPicture.appendChild(image);
+
+//             card.appendChild(townInfo);
+//             card.appendChild(townPicture);
+           
+//             document.querySelector('div.town-data').appendChild(card);
+//         }
+//       }
+//   });
+
+
